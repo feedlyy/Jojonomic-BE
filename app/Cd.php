dca003ec2cd4ce33783a9ab6302719d1eb153ac3
feedlyy <Milktank47>
2020-04-08 17:37:24 +0700

    created Cd model, migrations for users and cds

---
+++ app/Cd.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cd extends Model
+{
+    protected $table = 'cds';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+      'title', 'rate', 'category', 'quantity'
+    ];
+
+    public $rules = [
+      'title' => 'required|string',
+        'rate' => 'required|string',
+        'category' => 'required|in:action,cartoon,horror,romance',
+        'quantity' => 'required|integer'
+    ];
+}
---
+++ database/migrations/2020_04_08_083743_create_cds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cds', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('rate');
+            $table->enum('category', ['action', 'cartoon', 'horror', 'romance']);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cds');
+    }
+}
