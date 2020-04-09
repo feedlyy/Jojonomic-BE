ebe911ebb758b1314417c33a5c22f3028e094651
feedlyy <Milktank47>
2020-04-09 14:19:42 +0700

    update full access route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+//generate key for env
+$router->get('/key', function ($length = 32){
+    $char = '012345678dssd9abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charLen = strlen($char);
+
+    $getToken = '';
+    for ($i = 0; $i < $length; $i++) {
+        $getToken .= $char[rand(0, $charLen - 1)];
+    }
+
+    return $getToken;
+});
+
+//login user
+$router->put('/login', 'LoginController@login');
+
+$router->group(['middleware' => 'login'], function() use ($router) {
+    // get all items
+    $router->get('/cds', 'OwnerController@index');
+
+    // create new items
+    $router->post('/cd/create', 'OwnerController@store');
+
+    // update items
+    $router->put('/cd/{id}', 'OwnerController@update');
+
+    // rent a cd
+    $router->put('/rent/{id}', 'TransactionController@rentCd');
+
+    // logout user
+    $router->put('/logout', 'LoginController@logout');
+});
