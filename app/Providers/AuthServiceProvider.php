c14be019a955452f84942228c99c86fdc3f6dcb2
feedlyy <Milktank47>
2020-04-09 14:17:47 +0700

    write authorization for costumer / owner

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Gate::define('costumer', function ($user) {
+            return $user->role === 'costumer';
+        });
+        Gate::define('owner', function ($user) {
+            return $user->role === 'owner';
+        });
+
         // Here you may define how you wish users to be authenticated for your Lumen
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
